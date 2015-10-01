1fac473201c337d630ef0c703b42a456fca6e666
Marvin Dassios <marvindassios@web.de>
2015-10-01 23:56:43 +0200

    Updated SQL Dump again, aka Bugfix

--- SQL_dump.sql
+++ SQL_dump.sql
@@ -1,41 +1,20 @@
-CREATE TABLE CharacterProfile (
-CharacterProfileID int NOT NULL PRIMARY KEY UNIQUE,
-PlayerName varchar(256) NOT NULL,
-	FOREIGN KEY (PlayerName) REFERENCES Player(Nickname) ON DELETE CASCADE,
-Name varchar(256) NOT NULL,
-Age int NOT NULL,
-Gender varchar(256) NOT NULL,
-Job varchar(256),
-DateOfBirth varchar(256) NOT NULL,
-Appearance varchar(256)
-);
-
-CREATE TABLE CharacterStats (
-CharacterID int NOT NULL PRIMARY KEY UNIQUE,
-BodyStatsID int NOT NULL UNIQUE, 
-	FOREIGN KEY (BodyStatsID) REFERENCES BodyStats(BodyStatsID) ON DELETE CASCADE,
-SocialStatsID int NOT NULL UNIQUE,
-	FOREIGN KEY (SocialStatsID) REFERENCES SocialStats(SocialStatsID),
-IntellectualStatsID int NOT NULL UNIQUE,
-	FOREIGN KEY (IntellectualStatsID) REFERENCES IntellectualStats(IntellectualStatsID) ON DELETE CASCADE,
-SkillsID int NOT NULL UNIQUE,
-	FOREIGN KEY (SkillsID) REFERENCES Skills(SkillsID) ON DELETE CASCADE,
-WeaponSkillsID int NOT NULL UNIQUE,
-	FOREIGN KEY (WeaponSkillsID) REFERENCES WeaponSkills(WeaponSkillsID) ON DELETE CASCADE,
-InventoryID int NOT NULL UNIQUE,
-	FOREIGN KEY (InventoryID) REFERENCES Inventory(InventoryID) ON DELETE CASCADE,
-CurrentHealth int NOT NULL,
-CurrentThirst int NOT NULL,
-CurrentHunger int NOT NULL,
-CurrentSanity int NOT NULL
-);
-
 CREATE TABLE BodyStats (
 BodyStatsID int NOT NULL PRIMARY KEY UNIQUE,
 Dexterity int NOT NULL,
 Agility int NOT NULL,
 Strength int NOT NULL,
 Endurance int NOT NULL
+);
+
+CREATE TABLE Inventory (
+InventoryID int NOT NULL PRIMARY KEY,
+Slot varchar(255),
+Head varchar(255),
+Torso varchar(255),
+LeftHand varchar(255),
+RightHand varchar(255),
+Legs varchar(255),
+Feet varchar(255)
 );
 
 CREATE TABLE SocialStats (
@@ -86,31 +65,51 @@ Other int NOT NULL
 );
 
 CREATE TABLE Party (
-DungeonMasterName varchar(256) NOT NULL,
-	FOREIGN KEY (DungeonMasterName) REFERENCES Player(Name) ON DELETE CASCADE,
+DungeonMasterName varchar(255) NOT NULL,
 PartyID int NOT NULL PRIMARY KEY UNIQUE,
 NumberPlayer int NOT NULL,
 ThirstModifier int NOT NULL,
 HungerModifier int NOT NULL,
 SanityModifier int NOT NULL,
-CharacterSlot varchar(256)
+CharacterSlot varchar(255)
 );
 
 CREATE TABLE Player (
-Name varchar(256),
-Nickname varchar(256) NOT NULL PRIMARY KEY,
-Password varchar(256) NOT NULL,
+Name varchar(255),
+Nickname varchar(255) NOT NULL PRIMARY KEY,
+Password varchar(255) NOT NULL,
 PartySlot int,
 	FOREIGN KEY (PartySlot) REFERENCES Party(PartyID) ON DELETE CASCADE
 );
 
-CREATE TABLE Inventory (
-InventoryID int NOT NULL PRIMARY KEY,
-Slot varchar(256),
-Head varchar(256),
-Torso varchar(256),
-LeftHand varchar(256),
-RightHand varchar(256),
-Legs varchar(256),
-Feet varchar(256)
+CREATE TABLE CharacterProfile (
+CharacterProfileID int NOT NULL PRIMARY KEY UNIQUE,
+PlayerName varchar(255) NOT NULL,
+	FOREIGN KEY (PlayerName) REFERENCES Player(Nickname) ON DELETE CASCADE,
+Name varchar(255) NOT NULL,
+Age int NOT NULL,
+Gender varchar(255) NOT NULL,
+Job varchar(255),
+DateOfBirth varchar(255) NOT NULL,
+Appearance varchar(255)
+);
+
+CREATE TABLE CharacterStats (
+CharacterID int NOT NULL PRIMARY KEY UNIQUE,
+BodyStatsID int NOT NULL UNIQUE, 
+	FOREIGN KEY (BodyStatsID) REFERENCES BodyStats(BodyStatsID) ON DELETE CASCADE,
+SocialStatsID int NOT NULL UNIQUE,
+	FOREIGN KEY (SocialStatsID) REFERENCES SocialStats(SocialStatsID),
+IntellectualStatsID int NOT NULL UNIQUE,
+	FOREIGN KEY (IntellectualStatsID) REFERENCES IntellectualStats(IntellectualStatsID) ON DELETE CASCADE,
+SkillsID int NOT NULL UNIQUE,
+	FOREIGN KEY (SkillsID) REFERENCES Skills(SkillsID) ON DELETE CASCADE,
+WeaponSkillsID int NOT NULL UNIQUE,
+	FOREIGN KEY (WeaponSkillsID) REFERENCES WeaponSkills(WeaponSkillsID) ON DELETE CASCADE,
+InventoryID int NOT NULL UNIQUE,
+	FOREIGN KEY (InventoryID) REFERENCES Inventory(InventoryID) ON DELETE CASCADE,
+CurrentHealth int NOT NULL,
+CurrentThirst int NOT NULL,
+CurrentHunger int NOT NULL,
+CurrentSanity int NOT NULL
 );
