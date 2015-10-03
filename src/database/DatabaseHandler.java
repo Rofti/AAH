38c571bd6ee66acd35f4a79998b294a6cf72393d
Marven von Domarus <marvenvondomarus@gmx.de>
2015-10-03 13:02:04 +0200

    Added CRUD methods for Player
    Create , read , update, delete methods in DatabaseHandler.
    equals and hashCode method in Player.
    NOTE: deletePlayer needs update when CRUD methods for CharacterProfile
    is done

--- src/database/DatabaseHandler.java
+++ src/database/DatabaseHandler.java
@@ -3,9 +3,15 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.mysql.jdbc.Statement;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import utils.Player;
 
 public class DatabaseHandler {
 
@@ -13,6 +19,12 @@ public class DatabaseHandler {
 	private final String DATABASE_USER = "UserAAH";
 	private final String DATABASE_USERPW = "1234aah";
 
+	/**
+	 * Opens a Database connection to the Database defined in DATABASE_NAME with
+	 * the DATABASE_USER.
+	 * 
+	 * @return returns open Connection.
+	 */
 	private Connection openDataBase() {
 		Connection connect = null;
 		try {
@@ -41,6 +53,16 @@ public class DatabaseHandler {
 		return connect;
 	}
 
+	/**
+	 * Closes Connection, Statement and ResultSet
+	 * 
+	 * @param connect
+	 *            Connection to close.
+	 * @param stmt
+	 *            Statement to close.
+	 * @param resSet
+	 *            ResultSet to close.
+	 */
 	private void closeDatabase(Connection connect, Statement stmt,
 			ResultSet resSet) {
 		try {
@@ -87,6 +109,256 @@ public class DatabaseHandler {
 		}
 	}
 
-	
-	
+	public void createPlayer(Player player, String password) {
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "INSERT INTO Player " + "VALUES ( '"
+					+ player.getPlayerName() + "' , '" + player.getNickname()
+					+ "' , '" + password + "' , '"
+					+ player.getPartiesJSON().toString() + "' );";
+
+			stmt.executeUpdate(sqlQuery);
+
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+
+	}
+
+	public Player readPlayer(String nickname) {
+
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		Player player = null;
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "SELECT Name, Nickname, PartySlots FROM Player WHERE Nickname=\""
+					+ nickname + "\"";
+
+			resSet = stmt.executeQuery(sqlQuery);
+			if (resSet.next()) {
+				JSONArray partySlots = new JSONArray();
+				try {
+					partySlots = new JSONArray(resSet.getString("PartySlots"));
+				} catch (JSONException e) {
+					e.printStackTrace(System.err);
+					System.err.println("Message: " + e.getMessage());
+					System.err.println("Error when reading player "
+							+ resSet.getString("Nickname")
+							+ ". PartySlots could not be parsed.");
+				}
+				player = new Player(resSet.getString("Name"),
+						resSet.getString("Nickname"), partySlots);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+		return player;
+	}
+
+	public String readPlayerPassword(String nickname) {
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		String password = null;
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "SELECT Name, Nickname, PartySlots, Password FROM Player WHERE Nickname=\""
+					+ nickname + "\"";
+
+			resSet = stmt.executeQuery(sqlQuery);
+			if (resSet.next()) {
+				JSONArray partySlots = new JSONArray();
+				try {
+					partySlots = new JSONArray(resSet.getString("PartySlots"));
+				} catch (JSONException e) {
+					e.printStackTrace(System.err);
+					System.err.println("Message: " + e.getMessage());
+					System.err.println("Error when reading player "
+							+ resSet.getString("Nickname")
+							+ ". PartySlots could not be parsed.");
+				}
+				password = resSet.getString("Password");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+		return password;
+
+	}
+
+	public List<Player> readAllPlayerOfParty(int partyID) {
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		Player player = null;
+		ArrayList<Player> playerList = new ArrayList<Player>();
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "SELECT Name, Nickname, PartySlots FROM Player";
+
+			resSet = stmt.executeQuery(sqlQuery);
+			while (resSet.next()) {
+				JSONArray partySlots = new JSONArray();
+				try {
+					partySlots = new JSONArray(resSet.getString("PartySlots"));
+				} catch (JSONException e) {
+					e.printStackTrace(System.err);
+					System.err.println("Message: " + e.getMessage());
+					System.err.println("Error when reading player "
+							+ resSet.getString("Nickname")
+							+ ". PartySlots could not be parsed.");
+				}
+				player = new Player(resSet.getString("Name"),
+						resSet.getString("Nickname"), partySlots);
+				for(int i = 0; i< partySlots.length(); ++i){
+					if(partySlots.getInt(i)==partyID){
+						playerList.add(player);
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+		return playerList;
+	}
+
+	public void updatePlayer(Player player) {
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "UPDATE Player " + "SET Name='" + player.getPlayerName()
+					+ "' ," + "PartySlots='" + player.getPartiesJSON() + "' "
+					+ " WHERE Nickname='" + player.getNickname() + "';";
+
+			stmt.executeUpdate(sqlQuery);
+
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+	}
+
+	public void updatePlayerPassword(String nickname, String password) {
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "UPDATE Player " + "SET Password='" + password + "' "
+					+ " WHERE Nickname='" + nickname + "';";
+
+			stmt.executeUpdate(sqlQuery);
+
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+	}
+
+	public void deletePlayer(String nickname) {
+		Connection connect = null;
+		Statement stmt = null;
+		ResultSet resSet = null;
+		try {
+			connect = openDataBase();
+			stmt = connect.createStatement();
+
+			String sqlQuery;
+			sqlQuery = "DELETE FROM Player WHERE Nickname='" + nickname + "'";
+
+			stmt.executeUpdate(sqlQuery);
+
+			//TODO: DELETE ALL CHARACTER OF THIS PLAYER (database consistency)
+		} catch (SQLException e) {
+			e.printStackTrace(System.err);
+
+			System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+			System.err.println("Error Code: "
+					+ ((SQLException) e).getErrorCode());
+
+			System.err.println("Message: " + e.getMessage());
+		} finally {
+			closeDatabase(connect, stmt, resSet);
+		}
+
+	}
+
 }
--- src/utils/Player.java
+++ src/utils/Player.java
@@ -2,17 +2,19 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 
 public class Player {
-	
+
 	private String playerName;
 	private final String nickname;
 	/**
 	 * JSONArray that contains all the IDs of parties this player is part of.
 	 */
 	private JSONArray parties;
-	
+
 	public Player(String playerName, String nickname, JSONArray parties) {
 		super();
 		this.playerName = playerName;
@@ -33,13 +35,13 @@ public class Player {
 	}
 
 	public List<Integer> getPartiesList() {
-		ArrayList<Integer> partyList= new ArrayList<Integer>();
-		for(int i= 0; i< parties.length(); ++i){
+		ArrayList<Integer> partyList = new ArrayList<Integer>();
+		for (int i = 0; i < parties.length(); ++i) {
 			partyList.add(parties.getInt(i));
 		}
 		return partyList;
 	}
-	
+
 	public void setParties(JSONArray parties) {
 		this.parties = parties;
 	}
@@ -47,6 +49,42 @@ public class Player {
 	public String getNickname() {
 		return nickname;
 	}
-	
-	
+
+	@Override
+	public boolean equals(Object other) {
+		boolean result = false;
+		if (other == null) {
+			return result;
+		}
+		if (other instanceof Player) {
+			Player that = (Player) other;
+			result = (this.playerName.equals(that.playerName) && this.nickname
+					.equals(that.nickname));
+			if (result
+					&& this.getPartiesJSON().length() == that.getPartiesJSON()
+							.length()) {
+				for (int i = 0; i < this.getPartiesJSON().length(); i++) {
+					try {
+						if (this.getPartiesJSON().getInt(i) != that
+								.getPartiesJSON().getInt(i)) {
+							result = false;
+						}
+					} catch (JSONException e) {
+						e.printStackTrace(System.err);
+						System.err.println("Message: " + e.getMessage());
+						result = false;
+						break;
+					}
+				}
+			} else {
+				result = false;
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.getNickname().hashCode();
+	}
 }
